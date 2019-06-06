1ff48acf411b667ad3d044cba006af2b3a3929a7
LujieDuan <66813185@qq.com>
2019-06-06 00:37:57 -0600

    Test node connect to redis

--- googletrends/nodecollector/app.js
+++ googletrends/nodecollector/app.js
@@ -33,3 +33,21 @@ googleTrends.interestOverTime({keyword: config.keyword, startTime: config.start_
 .catch(function(err){
   console.error('Oh no there was an error', err);
 });
+
+var redis = require("redis"),
+client = redis.createClient("redis://redisdb:6379");
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+    client.quit();
+});
