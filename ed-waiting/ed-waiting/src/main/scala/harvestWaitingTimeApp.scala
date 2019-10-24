3e339289cf32d3934fb211e88abb03e935c433e0
LujieDuan <66813185@qq.com>
2019-10-24 12:05:29 -0600

    T - ed-waiting added error retry

--- ed-waiting/ed-waiting/src/main/scala/harvestWaitingTimeApp.scala
+++ ed-waiting/ed-waiting/src/main/scala/harvestWaitingTimeApp.scala
@@ -22,43 +22,55 @@ object harvestWaitingTimeApp extends App {
 
   while (true)
   {
-    val html = Source.fromURL("https://www.saskatoonhealthregion.ca/pages/emergency-wait-times.aspx", "ISO-8859-1").getLines.toStream
-    val table = "<table class=\"waittimetable\">((?!<table).)*</table>".r.findFirstIn(html.mkString
-                                                                          .replaceAll("\t", "")
-                                                                          .replaceAll("\n", "")
-                                                                          .replaceAll("\r", ""))
-    val lines = scala.xml.XML.loadString(table.get) \ "tbody" \ "tr"
-    val hospital_map = Map("JPCH Children's ED" -> "JPCH_Childrens_ED_patient",
-                            "Royal University Hospital Adult ED" -> "Royal_University_Hospital_patient",
-                            "Saskatoon City Hospital" -> "Saskatoon_City_Hospital_patient",
-                            "St Paul's Hospital" -> "St_Pauls_Hospital_patient")
+    var error_counter = 0
+    try 
+    {
+      val html = Source.fromURL("https://www.saskatoonhealthregion.ca/pages/emergency-wait-times.aspx", "ISO-8859-1").getLines.toStream
+      val table = "<table class=\"waittimetable\">((?!<table).)*</table>".r.findFirstIn(html.mkString
+                                                                            .replaceAll("\t", "")
+                                                                            .replaceAll("\n", "")
+                                                                            .replaceAll("\r", ""))
+      val lines = scala.xml.XML.loadString(table.get) \ "tbody" \ "tr"
+      val hospital_map = Map("JPCH Children's ED" -> "JPCH_Childrens_ED_patient",
+                              "Royal University Hospital Adult ED" -> "Royal_University_Hospital_patient",
+                              "Saskatoon City Hospital" -> "Saskatoon_City_Hospital_patient",
+                              "St Paul's Hospital" -> "St_Pauls_Hospital_patient")
 
-    val now = Calendar.getInstance().getTime().toString
-    println(s"waiting list was harvested at $now")
+      val now = Calendar.getInstance().getTime().toString
+      println(s"waiting list was harvested at $now")
 
-    if (lines.length == 0) println("NO DATE")
-    else {
-      var result = "LOCATION, COUNT, AVERAGE TIME, LONGEST TIME\n"
-      lines.foreach(x => {
-        val cells = x \ "td"
-        if (cells.length == 4) {
-          val cell_values = cells.map(c => c.toString().replaceAll("<td class=\"location\">", "")
-                                                            .replaceAll("<td>", "")
-                                                            .replaceAll("</td>", "")
-                                                            .trim())
-          val channel_name = hospital_map(cell_values(0))
-          publishToChannel(channel_name, if (cell_values(1) == "") "0.0" else cell_values(1))
-          result = result + "\"" + cell_values(0) + "\"," + cell_values(1) + "," + cell_values(2) + "," + cell_values(3) + "\n"
-        }
-      })
-      println(result)
-      val pw = new PrintWriter(new File("./waitingtimelist/waiting-time-" + now + ".csv" ))
-      pw.write(result)
-      pw.close()
+      if (lines.length == 0) println("NO DATE")
+      else {
+        var result = "LOCATION, COUNT, AVERAGE TIME, LONGEST TIME\n"
+        lines.foreach(x => {
+          val cells = x \ "td"
+          if (cells.length == 4) {
+            val cell_values = cells.map(c => c.toString().replaceAll("<td class=\"location\">", "")
+                                                              .replaceAll("<td>", "")
+                                                              .replaceAll("</td>", "")
+                                                              .trim())
+            val channel_name = hospital_map(cell_values(0))
+            publishToChannel(channel_name, if (cell_values(1) == "") "0.0" else cell_values(1))
+            result = result + "\"" + cell_values(0) + "\"," + cell_values(1) + "," + cell_values(2) + "," + cell_values(3) + "\n"
+          }
+        })
+        println(result)
+        val pw = new PrintWriter(new File("./waitingtimelist/waiting-time-" + now + ".csv" ))
+        pw.write(result)
+        pw.close()
+      }
+
+      val countMinutesBetweenHarvests = 15
+      val countSecondsBetweenHarvests = 60 * countMinutesBetweenHarvests
+      Thread.sleep(countSecondsBetweenHarvests * 1000)
+    } 
+    catch 
+    {
+      case x: Exception => {
+        println("Error-" + error_counter + ": " + x.getMessage)
+        error_counter += 1
+        Thread.sleep(10000)
+      }
     }
-
-    val countMinutesBetweenHarvests = 15
-    val countSecondsBetweenHarvests = 60 * countMinutesBetweenHarvests
-    Thread.sleep(countSecondsBetweenHarvests * 1000)
   }
 }
